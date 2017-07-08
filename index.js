f4e8e394f86ee10e9d5caa2285dd58dbf2b64793
Dmitry Sky <dmitry.sky@icloud.com>
2017-07-08 12:05:25 -0600

    add base64 tray icon to computer top bar

--- index.js
+++ index.js
@@ -1,149 +1,176 @@
-// window.js
-var path = require('path');
-var options = [
-  {
-    title: "Stop Squinting! (Basic)",
-    body: "Damn brah sup with those brows coming so close together?"
-  },
-  {
-    title: "Stop Squinting! (Image)",
-    body: "Damn brah sup with those brows coming so close together?",
-    icon: path.join(__dirname, 'squinting-emoji.jpg')
-  }
-]
+const {ipcRenderer} = require('electron')
 
-function doNotify(evt) {
-  if (evt.srcElement.id == "basic") {
-    new Notification(options[0].title, options[0]);
-  }
-  else if (evt.srcElement.id == "image") {
-    new Notification(options[1].title, options[1]);
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  let n = new Notification('You did it!', {
+    body: 'Nice work.'
+  })
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("basic").addEventListener("click", doNotify);
-  document.getElementById("image").addEventListener("click", doNotify);
+  // Tell the notification to show the menubar popup window on click
+  n.onclick = () => { ipcRenderer.send('show-window') }
+
 })
 
 
 
-// SDK Needs to create video and canvas nodes in the DOM in order to function
-// Here we are adding those nodes a predefined div.
-var divRoot = $("#affdex_elements")[0];
-var width = 640;
-var height = 480;
-var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
-//Construct a CameraDetector and specify the image width / height and face detector mode.
-var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
-
-//Enable detection of all Expressions, Emotions and Emojis classifiers.
-detector.detectAllEmotions();
-detector.detectAllExpressions();
-detector.detectAllEmojis();
-detector.detectAllAppearance();
-
-//Add a callback to notify when the detector is initialized and ready for runing.
-detector.addEventListener("onInitializeSuccess", function() {
-  log('#logs', "The detector reports initialized");
-  //Display canvas instead of video feed because we want to draw the feature points on it
-  $("#face_video_canvas").css("display", "block");
-  $("#face_video").css("display", "none");
-});
-
-function log(node_name, msg) {
-  $(node_name).append("<span>" + msg + "</span><br />")
-}
-
-//function executes when Start button is pushed.
-function onStart() {
-  if (detector && !detector.isRunning) {
-    $("#logs").html("");
-    detector.start();
-  }
-  log('#logs', "Clicked the start button");
-}
-
-//function executes when the Stop button is pushed.
-function onStop() {
-  log('#logs', "Clicked the stop button");
-  if (detector && detector.isRunning) {
-    detector.removeEventListener();
-    detector.stop();
-  }
-};
-
-//function executes when the Reset button is pushed.
-function onReset() {
-  log('#logs', "Clicked the reset button");
-  if (detector && detector.isRunning) {
-    detector.reset();
-
-    $('#results').html("");
-  }
-};
-
-//Add a callback to notify when camera access is allowed
-detector.addEventListener("onWebcamConnectSuccess", function() {
-  log('#logs', "Webcam access allowed");
-});
-
-//Add a callback to notify when camera access is denied
-detector.addEventListener("onWebcamConnectFailure", function() {
-  log('#logs', "webcam denied");
-  console.log("Webcam access denied");
-});
-
-//Add a callback to notify when detector is stopped
-detector.addEventListener("onStopSuccess", function() {
-  log('#logs', "The detector reports stopped");
-  $("#results").html("");
-});
-
-//Add a callback to receive the results from processing an image.
-//The faces object contains the list of the faces detected in an image.
-//Faces object contains probabilities for all the different expressions, emotions and appearance metrics
-detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
-  $('#results').html("");
-  log('#results', "Timestamp: " + timestamp.toFixed(2));
-  log('#results', "Number of faces found: " + faces.length);
-  if (faces.length > 0) {
-    log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
-    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
-      return val.toFixed ? Number(val.toFixed(0)) : val;
-    }));
-    log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
-    drawFeaturePoints(image, faces[0].featurePoints);
-  }
-
-  // My Jam
-  if(faces[0].expressions.browFurrow > 75) {
-    console.log("damn brah! Sup with those brows?")
-    document.getElementById("image").click()
-    // setTimeout(function(){console.log("setting timeout 5 seconds")}, 5000);
-  }
 
 
 
-});
 
-//Draw the detected facial feature points on the image
-function drawFeaturePoints(img, featurePoints) {
-  var contxt = $('#face_video_canvas')[0].getContext('2d');
 
-  var hRatio = contxt.canvas.width / img.width;
-  var vRatio = contxt.canvas.height / img.height;
-  var ratio = Math.min(hRatio, vRatio);
 
-  contxt.strokeStyle = "#FFFFFF";
-  for (var id in featurePoints) {
-    contxt.beginPath();
-    contxt.arc(featurePoints[id].x,
-      featurePoints[id].y, 2, 0, 2 * Math.PI);
-    contxt.stroke();
 
-  }
-}
+
+
+
+
+
+
+// // window.js
+// var path = require('path');
+// var options = [
+//   {
+//     title: "Stop Squinting! (Basic)",
+//     body: "Damn brah sup with those brows coming so close together?"
+//   },
+//   {
+//     title: "Stop Squinting! (Image)",
+//     body: "Damn brah sup with those brows coming so close together?",
+//     icon: path.join(__dirname, 'squinting-emoji.jpg')
+//   }
+// ]
+
+// function doNotify(evt) {
+//   if (evt.srcElement.id == "basic") {
+//     new Notification(options[0].title, options[0]);
+//   }
+//   else if (evt.srcElement.id == "image") {
+//     new Notification(options[1].title, options[1]);
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById("basic").addEventListener("click", doNotify);
+//   document.getElementById("image").addEventListener("click", doNotify);
+// })
+
+
+
+// // SDK Needs to create video and canvas nodes in the DOM in order to function
+// // Here we are adding those nodes a predefined div.
+// var divRoot = $("#affdex_elements")[0];
+// var width = 640;
+// var height = 480;
+// var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+// //Construct a CameraDetector and specify the image width / height and face detector mode.
+// var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
+
+// //Enable detection of all Expressions, Emotions and Emojis classifiers.
+// detector.detectAllEmotions();
+// detector.detectAllExpressions();
+// detector.detectAllEmojis();
+// detector.detectAllAppearance();
+
+// //Add a callback to notify when the detector is initialized and ready for runing.
+// detector.addEventListener("onInitializeSuccess", function() {
+//   log('#logs', "The detector reports initialized");
+//   //Display canvas instead of video feed because we want to draw the feature points on it
+//   $("#face_video_canvas").css("display", "block");
+//   $("#face_video").css("display", "none");
+// });
+
+// function log(node_name, msg) {
+//   $(node_name).append("<span>" + msg + "</span><br />")
+// }
+
+// //function executes when Start button is pushed.
+// function onStart() {
+//   if (detector && !detector.isRunning) {
+//     $("#logs").html("");
+//     detector.start();
+//   }
+//   log('#logs', "Clicked the start button");
+// }
+
+// //function executes when the Stop button is pushed.
+// function onStop() {
+//   log('#logs', "Clicked the stop button");
+//   if (detector && detector.isRunning) {
+//     detector.removeEventListener();
+//     detector.stop();
+//   }
+// };
+
+// //function executes when the Reset button is pushed.
+// function onReset() {
+//   log('#logs', "Clicked the reset button");
+//   if (detector && detector.isRunning) {
+//     detector.reset();
+
+//     $('#results').html("");
+//   }
+// };
+
+// //Add a callback to notify when camera access is allowed
+// detector.addEventListener("onWebcamConnectSuccess", function() {
+//   log('#logs', "Webcam access allowed");
+// });
+
+// //Add a callback to notify when camera access is denied
+// detector.addEventListener("onWebcamConnectFailure", function() {
+//   log('#logs', "webcam denied");
+//   console.log("Webcam access denied");
+// });
+
+// //Add a callback to notify when detector is stopped
+// detector.addEventListener("onStopSuccess", function() {
+//   log('#logs', "The detector reports stopped");
+//   $("#results").html("");
+// });
+
+// //Add a callback to receive the results from processing an image.
+// //The faces object contains the list of the faces detected in an image.
+// //Faces object contains probabilities for all the different expressions, emotions and appearance metrics
+// detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
+//   $('#results').html("");
+//   log('#results', "Timestamp: " + timestamp.toFixed(2));
+//   log('#results', "Number of faces found: " + faces.length);
+//   if (faces.length > 0) {
+//     log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
+//     log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
+//       return val.toFixed ? Number(val.toFixed(0)) : val;
+//     }));
+//     log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
+//       return val.toFixed ? Number(val.toFixed(0)) : val;
+//     }));
+//     log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
+//     drawFeaturePoints(image, faces[0].featurePoints);
+//   }
+
+//   // My Jam
+//   if(faces[0].expressions.browFurrow > 75) {
+//     console.log("damn brah! Sup with those brows?")
+//     document.getElementById("image").click()
+//     // setTimeout(function(){console.log("setting timeout 5 seconds")}, 5000);
+//   }
+
+
+
+// });
+
+// //Draw the detected facial feature points on the image
+// function drawFeaturePoints(img, featurePoints) {
+//   var contxt = $('#face_video_canvas')[0].getContext('2d');
+
+//   var hRatio = contxt.canvas.width / img.width;
+//   var vRatio = contxt.canvas.height / img.height;
+//   var ratio = Math.min(hRatio, vRatio);
+
+//   contxt.strokeStyle = "#FFFFFF";
+//   for (var id in featurePoints) {
+//     contxt.beginPath();
+//     contxt.arc(featurePoints[id].x,
+//       featurePoints[id].y, 2, 0, 2 * Math.PI);
+//     contxt.stroke();
+
+//   }
+// }
--- main.js
+++ main.js
@@ -1,8 +1,99 @@
-const {app, BrowserWindow} = require('electron');
+// const {app, BrowserWindow} = require('electron');
 
-let mainWindow;
+// let mainWindow;
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1200, height: 800});
-  mainWindow.loadURL('file://' + __dirname + '/window.html');
-});
+// app.on('ready', function() {
+//   mainWindow = new BrowserWindow({width: 1200, height: 800});
+//   mainWindow.loadURL('file://' + __dirname + '/window.html');
+// });
+
+
+
+
+
+const {app, BrowserWindow, ipcMain, Tray, nativeImage} = require('electron')
+const path = require('path')
+
+const assetsDir = path.join(__dirname, 'assets')
+
+let tray = undefined
+let window = undefined
+
+// This method is called once Electron is ready to run our code
+// It is effectively the main method of our Electron app
+app.on('ready', () => {
+  // Setup the menubar with an icon
+  let icon = nativeImage.createFromDataURL(base64Icon)
+  tray = new Tray(icon)
+
+  // Add a click handler so that when the user clicks on the menubar icon, it shows
+  // our popup window
+  tray.on('click', function(event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (window.isVisible() && process.defaultApp && event.metaKey) {
+      window.openDevTools({mode: 'detach'})
+    }
+  })
+
+  // Make the popup window for the menubar
+  window = new BrowserWindow({
+    width: 300,
+    height: 350,
+    show: false,
+    frame: false,
+    resizable: false,
+  })
+
+  // Tell the popup window to load our index.html file
+  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+
+  // Only close the window on blur if dev tools isn't opened
+  window.on('blur', () => {
+    if(!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
+})
+
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    showWindow()
+  }
+}
+
+const showWindow = () => {
+  const trayPos = tray.getBounds()
+  const windowPos = window.getBounds()
+  let x, y = 0
+  if (process.platform == 'darwin') {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height)
+  } else {
+    x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2))
+    y = Math.round(trayPos.y + trayPos.height * 10)
+  }
+
+
+  window.setPosition(x, y, false)
+  window.show()
+  window.focus()
+}
+
+ipcMain.on('show-window', () => {
+  showWindow()
+})
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// Tray Icon as Base64 so tutorial has less overhead
+let base64Icon = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAAAAAA6mKC9AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfhBwgOAiUV9A23AAAAyElEQVQY012Puw3CQBBErwRMCD0AiSUkQ0IZOEBY8uFOoAB+bRhogABIaQAowxh773F3JmKT0Y40u28UIIIfr8qq4TSNRvEJI84QXr223u0WQf9lFyU8W4lgDHUSPBFlCDWlqSpTkoYYRd4tWN7guqLoHuyNWQrZBtYZpDNrjDe8H0jNo2Q7boyq4agaw0bq+/lyOd/rJnLoFGTDyWSY2aO5Iw1TPLug3VsLGsw/DuyTtByYQ++39X6vg4FH9+WOcRTFx1+5v/pf1vbIWPe3y5UAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTctMDctMDhUMTQ6MDI6MzctMDQ6MDBt3WNxAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE3LTA3LTA4VDE0OjAyOjM3LTA0OjAwHIDbzQAAAABJRU5ErkJggg==`
